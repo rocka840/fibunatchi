20d765299d3193c1df0849679e2883cb6cc15b28
rocka840 <you@example.com>
2019-10-14 11:42:00 +0200

    xdgxhg

--- 4.js
+++
@@ -1,6 +0,0 @@
-for (i = 0; i < 100; i = i + 1) {
-    if ()
-        alert()
-}
-
-console.log(i);
---
+++ main.js
@@ -0,0 +1,8 @@
+var sum = 10;
+
+for (i = 0; i <= 10; i++) {
+
+    sum = sum + 1;
+
+}
+console.log(i)
